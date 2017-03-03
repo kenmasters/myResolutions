e2308a9a7513b2d034c1986704a1b2960dba8715
kenmasters <kjsiosan@gmail.com>
2017-03-03 21:57:14 +0800

    Some changes

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -8,9 +8,24 @@ import NotFound from '../404';
 
 // Home route
 FlowRouter.route('/', {
-	action() {
+	action: function() {
 		mount( MainLayout, { content: <App /> });
-	}
+	},
+
+	triggersEnter: [
+		function() { console.log(1); },
+		function() { console.log(2); }
+
+	]
+	//,() {
+	// 	action_A() {
+	// 		console.log(1);
+	// 	}
+
+	// 	action_B() {
+	// 		console.log(2);
+	// 	}
+	// }
 });
 
 // 404 route
